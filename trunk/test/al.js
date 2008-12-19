8a89623ce0db3cee4dc4190ed658ad94f750685e
edomontik <edomontik@50eb618e-b3be-11dd-9a91-ad6949c36eed>
2008-12-19 14:02:17 +0000

    l'ho commentato perche' a me dava problemi che non saprei risolvere

--- trunk/test/al.js
+++ trunk/test/al.js
@@ -1,23 +1,32 @@
 // l'idea e' di tenere in questo file
 // solo le funzioni di onsuccess e onerror
 
-var doc = document.implementation.createDocument();//sforna nodi xml
-
+//var doc = document.implementation.createDocument();//sforna nodi xml
+/*
 function getHome(){
-var obi = 
-{'url': document.location + "home.xml", 
-'onSuccess':function(rec){
-
-var appendi = rec.responseXML.documentElement;
-var layout = randLay(DFCORR);
-var skin = randSkin(DFCORR, layout);
-var tronco = acdf.assem(layout, skin);
-compose(tronco, "formatta/dati", new Array(appendi)); //qui ho il dom della home da mandare al formatto
-var xml = serializza(tronco);
-acdf.formatDoc(xml, DFCORR, homeHandl)}; //TODO homeHandl da definire. Deve rimpiazzare l'attuale body.
-AjaxRequest.get(obi);}
+    var obi = 
+    {
+        'url': document.location + "home.xml", 
+        'onSuccess':function(rec)
+        {
+            var appendi = rec.responseXML.documentElement;
+            var layout = randLay(DFCORR);
+            var skin = randSkin(DFCORR, layout);
+            var tronco = acdf.assem(layout, skin);
+            compose(tronco, "formatta/dati", new Array(appendi)); //qui ho il dom della home da mandare al formatto
+            var xml = serializza(tronco);
+            acdf.formatDoc(xml, DFCORR, homeHandl)
+        } //TODO homeHandl da definire. Deve rimpiazzare l'attuale body.
+    
+    }
+AjaxRequest.get(obi);
 };
+*/
 
+/*replyTo:
+ * scheda-> e' un documento DOM, la scheda a cui si intende rispondere
+ * reply-> contiene il body della risposta
+ */
 function replyTo (scheda, reply){
-	
+		
 }
